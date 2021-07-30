1f7cbdf8de05daf0548882a401619d1dda173ce2
Pranathi1092 <84650597+Pranathi1092@users.noreply.github.com>
2021-07-30 18:59:51 +0530

    Add files via upload

---
+++ build/precache-manifest.cf828c9b029a29a0fc5706f14e0c6138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd100c2ee7595bd07ea6f4cc220b17e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5bea491d6687563c38e",
+    "url": "/static/css/main.357de09d.chunk.css"
+  },
+  {
+    "revision": "91d06a3283a6642a9e8f",
+    "url": "/static/js/2.1cb2cad8.chunk.js"
+  },
+  {
+    "revision": "c5bea491d6687563c38e",
+    "url": "/static/js/main.3c80944a.chunk.js"
+  },
+  {
+    "revision": "562cdea7507386a61208",
+    "url": "/static/js/runtime-main.85a04175.js"
+  }
+]);
